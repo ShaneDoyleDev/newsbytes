835d7b976ec2ae61ff02d83647071f09d04e145e
Shane Doyle <shanedoyle1989@gmail.com>
2023-11-06 13:30:19 +0000

    feat(user_account.py): add instance properties

--- user_account.py
+++ user_account.py
@@ -1,2 +1,7 @@
 class UserAccount:
-    pass
+    """User with username and credits."""
+    def __init__(self, username):
+        self.username = username.title()
+        self.funds = 0.0
+        self.credits = 0
+        self.purchased_articles = []
