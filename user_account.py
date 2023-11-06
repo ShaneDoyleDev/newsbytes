25413939773464014f1f291ece77e8b9ed896571
Shane Doyle <shanedoyle1989@gmail.com>
2023-11-06 13:30:40 +0000

    feat(user_account.py): add add_funds method

--- user_account.py
+++ user_account.py
@@ -5,3 +5,7 @@ class UserAccount:
         self.funds = 0.0
         self.credits = 0
         self.purchased_articles = []
+
+    def add_funds(self, amount):
+        """Add funds to user's account."""
+        self.funds += round(amount, 2)
