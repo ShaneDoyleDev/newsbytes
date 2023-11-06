1605e251fb08699c2d15a16b5a6c4fbed15e975c
Shane Doyle <shanedoyle1989@gmail.com>
2023-11-06 13:43:29 +0000

    feat(currency.py): add class properties

--- currency.py
+++ currency.py
@@ -1,4 +1,19 @@
 import requests
 
 class Currency:
-    pass
+    """
+    Currency class for working with currency exchange rates and symbols.
+
+    This class allows you to fetch exchange rates for a specified currency
+    and retrieve the currency symbol based on the currency code.
+    """
+
+    BASE_URL = "https://v6.exchangerate-api.com/v6/"
+    CURRENCY_SYMBOLS = {
+        "USD": "$",
+        "EUR": "€",
+        "GBP": "£",
+        "CAD": "$",
+        "AUD": "$",
+        "CNY": "¥"
+    }
