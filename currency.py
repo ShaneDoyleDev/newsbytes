44438e0b0c2eeef0280ce0ed6656705587c19719
Shane Doyle <shanedoyle1989@gmail.com>
2023-11-13 12:27:07 +0000

    feat(currency.py): create fetch_exchange_rate private method

--- currency.py
+++ currency.py
@@ -27,3 +27,26 @@ class Currency:
     def get_symbol(self, currency_code):
         """Retrieve the currency symbol based on the currency code."""
         return self.CURRENCY_SYMBOLS.get(currency_code, "")
+
+    def _fetch_exchange_rate(self, currency_code):
+        """Private method to fetch the exchange rate for a specified amount between USD and selected currency type."""
+        url = f"{self.BASE_URL}{self.api_key}/pair/USD/{currency_code}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+
+            if data['result'] == 'error':
+                raise Exception(data['error-type'])
+
+            return data['conversion_rate']
+
+        except requests.exceptions.Timeout:
+            raise Exception("Request timed out. Please try again.")
+
+        except requests.exceptions.ConnectionError:
+            raise Exception("A network problem occurred. Please check your connection.")
+
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"An error occurred while fetching the exchange rate. Error: {e}")
